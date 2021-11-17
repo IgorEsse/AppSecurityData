7482d3417b144d58df33db2a6fdc1f3adaa3ce42
IgorEsse <esseigor@gmail.com>
2021-11-17 23:39:47 +0400

    Init commit

---
+++ AppSecurityData/src/main/java/ru/esseigor/appSecurityData/AppSecurityDataApplication.java
@@ -0,0 +1,13 @@
+package ru.esseigor.appSecurityData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSecurityDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSecurityDataApplication.class, args);
+	}
+
+}
---
+++ AppSecurityData/src/test/java/ru/esseigor/appSecurityData/AppSecurityDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.esseigor.appSecurityData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSecurityDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
