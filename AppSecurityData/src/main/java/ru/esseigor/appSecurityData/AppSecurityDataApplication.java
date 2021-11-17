899f9b2de2827803e29f9e241402bea09210b2f7
IgorEsse <esseigor@gmail.com>
2021-11-18 00:55:43 +0400

    add spring security and data

--- AppSecurityData/src/main/java/ru/esseigor/appSecurityData/AppSecurityDataApplication.java
+++ AppSecurityData/src/main/java/ru/esseigor/appSecurityData/AppSecurityDataApplication.java
@@ -2,11 +2,17 @@ package ru.esseigor.appSecurityData;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("ru.esseigor.appSecurityData.repo") 
+@EntityScan("ru.esseigor.appSecurityData.model")
+//@ComponentScan ("ru.esseigor.persistence.repo")
 @SpringBootApplication
 public class AppSecurityDataApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable {
 		SpringApplication.run(AppSecurityDataApplication.class, args);
 	}
 
